f32d91e96526514f0f7a23c36492e72263259a45
stephane kibonge <stephane.kibonge@etion.co.za>
2020-02-25 22:50:35 +0200

    Get book

--- introduction/introduction/Program.cs
+++ introduction/introduction/Program.cs
@@ -8,11 +8,16 @@ namespace introduction
     {
         static async Task Main(string[] args)
         {
+            string url = "http://www.gutenberg.org/files/2600/2600-0.txt";
+            var watch = System.Diagnostics.Stopwatch.StartNew();
             using (var client = new HttpClient())
             {
-                var content = await client.GetStringAsync("http://webcode.me");
-                Console.WriteLine(content);
+                var content = await client.GetStringAsync(url);
+                //Console.WriteLine(content);
             }
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+            Console.WriteLine($"Request took: {(elapsedMs/1000).ToString("F2")}secs");
         }
     }
 }

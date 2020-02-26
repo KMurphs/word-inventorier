2f9198db0735250cc0182e7d7e0650d6b96f1fa6
stephane kibonge <stephane.kibonge@etion.co.za>
2020-02-26 21:06:13 +0200

    Add Sanitize function for book input

---
+++ book-processor/BookProcessor/Processor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BookProcessor
+{
+    public class Processor
+    {
+        public string Sanitize(string inStr){
+            Regex r = new Regex("[^a-z0-9- ]", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+            return r.Replace(inStr, "");
+        }
+    }
+}

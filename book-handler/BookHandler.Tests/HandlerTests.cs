e4b7ce45a4c43f25a65ea79081db83a68d4a50a0
stephane kibonge <stephane.kibonge@etion.co.za>
2020-02-27 16:41:46 +0200

    Fix compilte errors

--- book-handler/BookHandler.Tests/HandlerTests.cs
+++ book-handler/BookHandler.Tests/HandlerTests.cs
@@ -1,17 +1,25 @@
 using NUnit.Framework;
+using BookHandler;
+using BookTypes;
+using System.Collections.Generic;
+
 
 namespace BookHandler.Tests
 {
     public class HandlerTests
     {
+        private Handler bookHandler;
+
         [SetUp]
         public void Setup()
         {
+            bookHandler = new Handler();
         }
 
         [Test]
-        public void Test1()
+        public void CanHandleANewBookFromRetrievalUntilQueryResult()
         {
+
             Assert.Pass();
         }
     }
--- book-handler/BookHandler/Handler.cs
+++ book-handler/BookHandler/Handler.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Text;
 using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using BookProcessor;
 using BookTypes;
 using BookInventorier;
@@ -12,15 +15,17 @@ namespace BookHandler
 
     public class Handler
     {
+        private Processor bookProcessor;
+        private Inventorier bookInventorier;
         public async Task<BookSummary> Handle(string bookSrc, int topN_inFreq = 50, int topN_inFreqLongerthan_L = 50, int Length_L = 6){
             BookSummary bookSummary;
-            Processor bookProcessor = new Processor();
-            Inventorier bookInventorier = new Inventorier();
+            this.bookProcessor = new Processor();
+            this.bookInventorier = new Inventorier();
             string sanitizedBook = "";
             string bookID = "";
 
             // Get and sanitize book
-            if(bookSrc.IndexOf("http")){
+            if(bookSrc.IndexOf("http") != -1){
                 sanitizedBook = await bookProcessor.FetchCorpus(bookSrc, true);
                 bookID = bookSrc;
             }else{
@@ -29,18 +34,18 @@ namespace BookHandler
             }
 
             // Perform inventory on book
-            Dictionary<string, int> freqs;
-            Dictionary<int, LinkedList<string>> lengths;
-            IRecordUnit mostFrequentToken;
-            IRecordUnit longestToken;
+            IDictionary<string, int> freqs;
+            IDictionary<int, LinkedList<string>> lengths;
+            IInventoryItem mostFrequentToken;
+            IInventoryItem longestToken;
             int bookLength = 0;
-            double durationMs = bookInventorier.Process(sanitizedBook, out freqs, out lengths, out bookLength, out mostFrequentToken, out longestToken);
+            double durationMs = bookInventorier.Process(sanitizedBook, out freqs, out lengths, out mostFrequentToken, out longestToken, out bookLength);
 
 
             // Query Inventory against query parameters
             List<IQueryResult> results = new List<IQueryResult>();
             results.Add(QueryWrapper(freqs, lengths, 0, topN_inFreq));
-            results.Add(QueryWrapper(freqs, Length_L, 0, topN_inFreqLongerthan_L));
+            results.Add(QueryWrapper(freqs, lengths, Length_L, topN_inFreqLongerthan_L));
 
             
 
@@ -49,21 +54,21 @@ namespace BookHandler
                 bookID,
                 bookLength,
                 freqs.Count,
+                mostFrequentToken, 
+                longestToken, 
                 durationMs/1000, 
-                mostFrequentWord, 
-                longestWord, 
                 results
             );
             return bookSummary;
 
         }
-        public QueryResult QueryWrapper(IDictionary<string, int> tokenToFreqDict, IDictionary<int, LinkedList<string>> lenghtToTokenDict, int minLength, int topNCount)
+        public IQueryResult QueryWrapper(IDictionary<string, int> tokenToFreqDict, IDictionary<int, LinkedList<string>> lenghtToTokenDict, int minLength, int topNCount)
         {
             List<IInventoryItem> results;
             double durationMs = bookInventorier.Query(tokenToFreqDict, lenghtToTokenDict, minLength, topNCount, out results);
             QueryResult res = new QueryResult(durationMs, results);
 
-            return res;
+            return (IQueryResult)res;
         }
 
 

2e5ac0bdbf8355baa4fa43514b5b32d0714e53df
stephane kibonge <stephane.kibonge@etion.co.za>
2020-02-27 21:26:29 +0200

    Move endpoint to api/[controller]

---
+++ web-app/WebApp/Controllers/CorpusInventoryController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CorpusInventoryController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CorpusInventoryController> _logger;
+
+        public CorpusInventoryController(ILogger<CorpusInventoryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<CorpusInventory> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new CorpusInventory
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}

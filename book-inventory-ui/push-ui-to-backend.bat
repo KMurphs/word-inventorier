b4b120f68d70d76df237b63cc07f43af454aaf54
stephane kibonge <stephane.kibonge@etion.co.za>
2020-02-28 11:40:51 +0200

    Push to heroku

---
+++ book-inventory-ui/push-ui-to-backend.bat
@@ -0,0 +1,34 @@
+@echo off
+
+set arg1=%1
+if '%arg1%'=='build' (
+  echo Build Ui Project
+  npm run build
+  echo Done
+  goto :Copy
+)
+
+:Copy
+echo Copying Files Over
+robocopy build ..\web-app\WebApp\wwwroot
+
+
+cd ..\web-app\WebApp
+dotnet build -c Release
+dotnet publish -c Release
+
+REM Docker must be up
+REM Dockerfile must be in publish directory
+heroku container:login
+heroku apps:create corpus-inventory
+docker build -t corpus-inventory C:\\PersonalProjects\\corpus-word-inventory\\web-app\\WebApp\\bin\\Release\\netcoreapp3.1\\publish
+docker tag corpus-inventory registry.heroku.com/corpus-inventory/web
+docker push registry.heroku.com/corpus-inventory/web
+heroku container:release web --app corpus-inventory
+heroku apps:open -a corpus-inventory
+
+
+REM While Testing
+docker build -t corpus-inventory C:\\PersonalProjects\\corpus-word-inventory\\web-app\\WebApp\\bin\\Release\\netcoreapp3.1\\publish
+docker push registry.heroku.com/corpus-inventory/web
+heroku apps:open -a corpus-inventory
---
+++ web-app/WebApp/bin/Debug/netcoreapp3.1/publish/wwwroot/precache-manifest.925aa4cf803ab25f2104ae1263311899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44104e1a6c3a9c18c69f3bb6393d7cbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2200fca09f30db548bd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ae9b72634e2e3e5be238",
+    "url": "/static/js/2.ca1af600.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ca1af600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2200fca09f30db548bd",
+    "url": "/static/js/main.ca050b9d.chunk.js"
+  },
+  {
+    "revision": "08e27906eea6827fb7d7",
+    "url": "/static/js/runtime-main.11ce2fe7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

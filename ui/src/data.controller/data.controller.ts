9c13fc2ed90d23daf138919ca8754027d57a46b1
stephane kibonge <stephane.kibonge@etion.co.za>
2020-11-15 21:22:25 +0200

    Added comments to data controller

--- ui/src/data.controller/data.controller.ts
+++ ui/src/data.controller/data.controller.ts
@@ -6,66 +6,81 @@ import { TUIQuery, TTextSummary, typeConverter } from "./data.types";
 
 
 export default class DataController {
-  // private urlBaseDevelopment: string = "https://localhost:5051/";
   private urlBaseDevelopment: string = "http://localhost:5050/";
-  // private urlBaseProduction: string = "https://corpus-inventory.herokuapp.com/";
-  // private urlBaseProduction: string = `${window.location.origin}/`; //Prevent the cors error between http and https
   private urlBase: string = "";
   private urlGetAllTexts: string = "api/corpusinventory";
-  // private urlGetAText: string = "api/corpusinventory/{id}";
   private urlProcessAText: string = "api/corpusinventory";
 
-  // private setUiData: Function
-  // private timerID: any
-  // private dataRefreshRateSec: number = 15;
+
+
   getVersion(){
     return '1.1'
   }
-  // constructor(setUiData: Function){
+
+
+
   constructor(){
-    // Determine whether we shoudl use dev urls or production urls at heroku
-    // if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    //   console.warn("[DC]: App is in Development Mode");
-    //   this.urlBase = this.urlBaseDevelopment;
-    // } else {
-    //   console.warn("[DC]: App is in Production Mode");
-    //   this.urlBase = this.urlBaseProduction;
-    // }
-    // this.urlBase = this.urlBaseDevelopment;
-    // this.urlBase = this.urlBaseProduction;
     this.urlBase = `${window.location.origin}/`;
     if(this.urlBase.indexOf("localhost") !== -1){
       this.urlBase = this.urlBaseDevelopment
     }
-    // this.setUiData = setUiData
-
-    // const _this = this
-    // this.timerID = setInterval(()=>{
-    //   _this.getTexts()
-    // }, this.dataRefreshRateSec)
-
   }
   
   
 
 
-  // Makes sure we are talking to right backend
-  buildURL(url: string){
-    return `${this.urlBase}${url}`
+
+  /**
+   * Ensures we are talking to right backend (dev url or production url)
+   * 
+   * @param  {string} uri: Endpoint of interest for a particular request
+   */
+  buildURL(uri: string){
+    return `${this.urlBase}${uri}`
   }
 
-
-
-
-
+  /**
+   * Retrieves the complete list of processing results of texts previously submitted to the backend
+   * 
+   * @returns Promise\<TTextSummary[]\>
+   */
   async getTexts(): Promise<TTextSummary[]>{
     let rawTexts = await httpRequest(this.buildURL(this.urlGetAllTexts));
     return (rawTexts as any[]).map((rawText: any) => typeConverter.toTextSummary(rawText))
   }
+  
+  /**
+   * Function will retrieve Processing Results from previously suubmitted text
+   * This text would be identified by an id that is passed as argument to this function
+   * 
+   * @param  {string} textId
+   * @returns Promise\<TTextSummary|null\>
+   */
   async getTextById(textId: string): Promise<TTextSummary|null>{
     let rawText = await httpRequest(`${this.buildURL(this.urlGetAllTexts)}/${textId}`);
     return typeConverter.toTextSummary(rawText);
   }
+
+  /**
+   * This function will receive a query data structure and will return a new data structure formed with the server response to the request
+   * 
+   * Example Call:
+   * 
+   * const text = await dc.processText({
+   *    text: "Where is my my umbrella",
+   *    queries: [{
+   *        minLength: 0,
+   *        maxLength: 3,
+   *        topN: 2
+   *    }]
+   * }, cb);
+   * 
+   * .
+   * 
+   * @param  {TUIQuery} uiQuery: Text to process and Array of query items i.e. [{ minLength: number, maxLength: number, topN: number }]
+   * @param  {Function} cb: Callback - Called when server response has been received and successfully processed
+   * @returns Promise\<TTextSummary\>
+   */
   processText(uiQuery: TUIQuery, cb: Function): Promise<TTextSummary>{
     return new Promise(async (resolve, reject) => {
       await httpRequest(`${this.buildURL(this.urlProcessAText)}`, uiQuery, "POST")
